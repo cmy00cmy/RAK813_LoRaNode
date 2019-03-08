9c02400ae835ba78655301f45865067a42fd1eb1
nero <nero.du@rakwireless.com>
2019-03-08 10:07:40 +0800

    hex

--- Apps/LoRaWAN_Demo/app_lora.c
+++ Apps/LoRaWAN_Demo/app_lora.c
@@ -804,9 +804,9 @@ static void MlmeConfirm( MlmeConfirm_t *mlmeConfirm )
                 DeviceState = DEVICE_STATE_SEND;
                 printf("OTAA Join Success \r\n");
 								low_power_send();
-								nrf_delay_ms(2000);
-							  SX1276SetSleep( );
-							  (void)sd_power_system_off();
+								//nrf_delay_ms(2000);
+							  //SX1276SetSleep( );
+							 // (void)sd_power_system_off();
             }
             else
             {
